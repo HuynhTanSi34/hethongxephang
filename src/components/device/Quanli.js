a1c2e03d5ba15d2daff549674a00a5bed6b52151
Huỳnh Tấn Sĩ <95234782+HuynhTanSi34@users.noreply.github.com>
2022-02-14 12:02:05 +0700

    Add files via upload

--- src/components/device/Quanli.js
+++ src/components/device/Quanli.js
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 import "../UI/style.css";
+import { useEffect, useReducer } from "react";
+import React from "react";
+import { Provider } from "react-redux";
+import leader from "../../redux/decivelist";
+import DeciveList from "../../components/device/devicelist";
 import classes from "../UI/DeviceModule/quanli.module.css";
 import Logomini from "../../components/image/Logomini.png";
 import filogout from "../../components/image/filogout.png";
@@ -16,8 +21,6 @@ import Bell from "../login/bell";
 import down from "../../components/image/down.png";
 import search from "../../components/image/search.png";
 import addsquare from "../../components/image/addsquare.png";
-import chamdo from "../../components/image/chamdo.png";
-import chamxanh from "../../components/image/chamxanh.png";
 import pastpage from "../../components/image/pastpage.png";
 import nextpage from "../../components/image/nextpage.png";
 
@@ -25,123 +28,82 @@ import { useState } from "react";
 function Quanli() {
   const [show, setShow] = useState(false);
   return (
-    <section className="screen">
-      <section className={classes.inform}>
-        <img src={Logomini} alt="Logo" className="logo1" />
-        <div className={classes.list}>
-          <div className={classes.listItem}>
-            <Link to="/dashboard/day" className="Link">
-              <img src={element4} />
-              <div>Dashboard</div>
-            </Link>
-          </div>
-          <div className={`${classes.listItem} ${classes.thiet}`}>
-            <img src={monitor} />
-            <div>Thiết bị</div>
-          </div>
-          <div className={classes.listItem}>
-            <img src={service} />
-            <div>Dịch vụ</div>
-          </div>
-          <div className={classes.listItem}>
-            <img src={filayers} />
-            <div>Cấp số</div>
-          </div>
-          <div className={classes.listItem}>
-            <Link to="/report" className="Link">
-              <img src={monitor} />
-              <div>Báo cáo</div>
-            </Link>
-          </div>
-          <div className={`${classes.listItem} ${classes.ml}`}>
-            <img src={setting} />
-            <div>Cài đặt hệ thống</div>
-            <img src={bacham} className={classes.bacham} />
-            <div className={classes.mucluc}>
-              <div className={classes.muclucItem}>Quản lí vai trò</div>
-              <div className={classes.muclucItem}>Quản lí tài khoản</div>
-              <div className={classes.muclucItem}>Nhật ký người dùng</div>
+    <Provider store={leader}>
+      <section className="screen">
+        <section className={classes.inform}>
+          <img src={Logomini} alt="Logo" className="logo1" />
+          <div className={classes.list}>
+            <div className={classes.listItem}>
+              <Link to="/dashboard/day" className="Link">
+                <img src={element4} />
+                <div>Dashboard</div>
+              </Link>
             </div>
-          </div>
-        </div>
-        <Link to={"/" || ""}>
-          <div className={classes.out}>
-            <img src={filogout} alt="out" className={classes.item5} />
-            <div>Đăng xuất</div>
-          </div>
-        </Link>
-      </section>
-      <section className={classes.content}>
-        <div className={classes.title}>Thiết bị {">"} </div>
-        <div className={classes.title1}>Danh sách thiết bị</div>
-        <div className={classes.title2}>Danh sách thiết bị</div>
-        <div className={classes.title3}>
-          <div>
-            <button onClick={() => setShow(!show)} className="bambell">
-              <img alt="thongbao" src={bell} className="bell" />
-              {show && <Bell />}
-            </button>
-          </div>
-          <div>
-            <Link to="/login/profile" className="Link">
-              <img alt="avatar" src={avatarmini} className="avatarmini" />
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/login/profile"
-              className={`${classes.linkavatar} ${classes.Link}`}
-            >
-              <div className="chao">Xin chào</div>
-              <div className="ten">Lê Quỳnh Ái Vân</div>
-            </Link>
-          </div>
-        </div>
-        <div className={classes.trangthaihoatdong}>
-          <div className={classes.trangthaihoatdongname}>
-            Trạng thái hoạt động
-          </div>
-          <div className={`${classes.trangthaihoatdongselec} ${classes.trs}`}>
-            Tất cả <img src={down} />
-            <div className={`${classes.trangthaihoatdonglist} ${classes.trl}`}>
-              <div className={classes.trangthaihoatdongitem}>Tất cả</div>
-              <div className={classes.trangthaihoatdongitem}>Hoạt động</div>
-              <div className={classes.trangthaihoatdongitem}>
-                Ngưng hoạt động
+            <div className={`${classes.listItem} ${classes.thiet}`}>
+              <img src={monitor} />
+              <div>Thiết bị</div>
+            </div>
+            <div className={classes.listItem}>
+              <img src={service} />
+              <div>Dịch vụ</div>
+            </div>
+            <div className={classes.listItem}>
+              <img src={filayers} />
+              <div>Cấp số</div>
+            </div>
+            <div className={classes.listItem}>
+              <Link to="/report" className="Link">
+                <img src={map} />
+                <div>Báo cáo</div>
+              </Link>
+            </div>
+            <div className={`${classes.listItem} ${classes.ml}`}>
+              <img src={setting} />
+              <div>Cài đặt hệ thống</div>
+              <img src={bacham} className={classes.bacham} />
+              <div className={classes.mucluc}>
+                <div className={classes.muclucItem}>Quản lí vai trò</div>
+                <div className={classes.muclucItem}>Quản lí tài khoản</div>
+                <div className={classes.muclucItem}>Nhật ký người dùng</div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={classes.trangthaiketnoi}>
-          <div className={classes.trangthaihoatdongname}>
-            Trạng thái kết nối
-          </div>
-          <div className={`${classes.trangthaihoatdongselec} ${classes.trs}`}>
-            Tất cả <img src={down} />
-            <div className={`${classes.trangthaihoatdonglist} ${classes.trl}`}>
-              <div className={classes.trangthaihoatdongitem}>Tất cả</div>
-              <div className={classes.trangthaihoatdongitem}>Kết nối</div>
-              <div className={classes.trangthaihoatdongitem}>Mất kết nối</div>
+          <Link to={"/" || ""}>
+            <div className={classes.out}>
+              <img src={filogout} alt="out" className={classes.item5} />
+              <div>Đăng xuất</div>
+            </div>
+          </Link>
+        </section>
+        <section className={classes.content}>
+          <div className={classes.title}>Thiết bị {">"} </div>
+          <div className={classes.title1}>Danh sách thiết bị</div>
+          <div className={classes.title2}>Danh sách thiết bị</div>
+          <div className={classes.title3}>
+            <div>
+              <button onClick={() => setShow(!show)} className="bambell">
+                <img alt="thongbao" src={bell} className="bell" />
+                {show && <Bell />}
+              </button>
+            </div>
+            <div>
+              <Link to="/login/profile" className="Link">
+                <img alt="avatar" src={avatarmini} className="avatarmini" />
+              </Link>
+            </div>
+            <div>
+              <Link
+                to="/login/profile"
+                className={`${classes.linkavatar} ${classes.Link}`}
+              >
+                <div className="chao">Xin chào</div>
+                <div className="ten">Lê Quỳnh Ái Vân</div>
+              </Link>
             </div>
           </div>
-        </div>
-        <div className={classes.tukhoa}>
-          <div className={classes.trangthaihoatdongname}>Từ khóa</div>
 
-          <input
-            type="text"
-            placeholder="Nhập từ khóa"
-            className={classes.trangthaihoatdongselec}
-          />
-          <img src={search} className={classes.search} />
-        </div>
-        <Link to="/device/adddevice">
-          <div className={classes.addnewdevice}>
-            <img src={addsquare} />
-            <div className={classes.addnewdevicecontent}>Thêm thiết bị</div>
-          </div>
-        </Link>
-        <section className={classes.table}>
+          <DeciveList />
+          {/* <section className={classes.table}>
           <table className={classes.bang}>
             <tr>
               <th className={classes.th} style={{ width: "103px" }}>
@@ -353,24 +315,25 @@ function Quanli() {
               <td className={`${classes.td} ${classes.duongdan}`}>Cập nhật</td>
             </tr>
           </table>
+        </section> */}
+          <div className={classes.newpage}>
+            <div className={classes.newpageitem}>
+              <img src={pastpage} />
+            </div>
+            <div className={`${classes.newpageitem} ${classes.ninow}`}>1</div>
+            <div className={classes.newpageitem}>2</div>
+            <div className={classes.newpageitem}>3</div>
+            <div className={classes.newpageitem}>4</div>
+            <div className={classes.newpageitem}>5</div>
+            <div className={classes.newpageitem}>...</div>
+            <div className={classes.newpageitem}>10</div>
+            <div className={classes.newpageitem}>
+              <img src={nextpage} />
+            </div>
+          </div>
         </section>
-        <div className={classes.newpage}>
-          <div className={classes.newpageitem}>
-            <img src={pastpage} />
-          </div>
-          <div className={`${classes.newpageitem} ${classes.ninow}`}>1</div>
-          <div className={classes.newpageitem}>2</div>
-          <div className={classes.newpageitem}>3</div>
-          <div className={classes.newpageitem}>4</div>
-          <div className={classes.newpageitem}>5</div>
-          <div className={classes.newpageitem}>...</div>
-          <div className={classes.newpageitem}>10</div>
-          <div className={classes.newpageitem}>
-            <img src={nextpage} />
-          </div>
-        </div>
       </section>
-    </section>
+    </Provider>
   );
 }
 export default Quanli;
--- src/components/device/devicelist.tsx
+++ src/components/device/devicelist.tsx
@@ -1,70 +1,220 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchLists } from "../../redux/decive/deciveActions";
+import { Link } from "react-router-dom";
+import Dropdown from "react-dropdown";
 import "../UI/style.css";
 import classes from "../UI/DeviceModule/quanli.module.css";
 import chamdo from "../../components/image/chamdo.png";
 import chamxanh from "../../components/image/chamxanh.png";
+import addsquare from "../../components/image/addsquare.png";
+import down from "../../components/image/down.png";
+import search from "../../components/image/search.png";
 function DeciveList({ listData, fetchLists }: any) {
   useEffect(() => {
-    setTimeout(() => {
-      fetchLists();
-    }, 0);
+    fetchLists();
   }, []);
+  const ServiceActivePoint = (value: any) => {
+    return (
+      <>
+        <span className="ActivePoint">
+          <img src={chamxanh} />
+        </span>
+        <span> Hoạt động</span>
+      </>
+    );
+  };
+  const ServiceUnactivePoint = (value: any) => {
+    return (
+      <>
+        <span className="UnActivePoint">
+          <img src={chamdo} />
+        </span>
+        <span>Ngưng hoạt động</span>
+      </>
+    );
+  };
+  const ServiceActivePoint1 = (value: any) => {
+    return (
+      <>
+        <span className="ActivePoint">
+          <img src={chamxanh} />
+        </span>
+        <span> Kết nối</span>
+      </>
+    );
+  };
+  const ServiceUnactivePoint1 = (value: any) => {
+    return (
+      <>
+        <span className="UnActivePoint">
+          <img src={chamdo} />
+        </span>
+        <span>Mất kết nối</span>
+      </>
+    );
+  };
+  const data = listData.list;
+  const [list, setList] = useState(data);
+  const options = ["Tất cả", "Hoạt động", "Ngưng hoạt động"];
+  const defaultOption = options[0];
+  const optionsConnect = ["Tất cả", "Kết nối", "Mất kết nối"];
+  const handleDropdownValue = (e: any) => {
+    if (e.value == "Hoạt động") {
+      const filterdata = data.filter(
+        (list: any) => list.activeStatus == "Hoạt động"
+      );
+      setList(filterdata);
+    } else if (e.value == "Ngưng hoạt động") {
+      const filterdata = data.filter(
+        (list: any) => list.activeStatus == "Ngưng hoạt động"
+      );
+      setList(filterdata);
+    } else if (e.value == "Ngưng hoạt động") {
+      const filterdata = data.filter(
+        (list: any) => list.activeStatus == "Ngưng hoạt động"
+      );
+      setList(filterdata);
+    } else if (e.value == "Kết nối") {
+      const filterdata = data.filter(
+        (list: any) => list.connecting == "Kết nối"
+      );
+      setList(filterdata);
+    } else if (e.value == "Mất kết nối") {
+      const filterdata = data.filter(
+        (list: any) => list.connecting == "Mất kết nối"
+      );
+      setList(filterdata);
+    } else {
+      setList(data);
+    }
+  };
+  const [showMore, setShowMore] = useState(false);
+
+  const [search, setSearch] = useState("");
   return (
-    <section className={classes.table}>
-      <table className={classes.bang}>
-        <tr>
-          <th className={classes.th} style={{ width: "103px" }}>
-            Mã thiết bị
-          </th>
-          <th className={classes.th} style={{ width: "99px" }}>
-            Tên thiết bị
-          </th>
-          <th className={classes.th} style={{ width: "136px" }}>
-            Địa chỉ IP
-          </th>
-          <th className={classes.th} style={{ width: "174px" }}>
-            Trạng thái hoạt động
-          </th>
-          <th className={classes.th} style={{ width: "145px" }}>
-            Trạng thái kết nối
-          </th>
-          <th className={classes.th} style={{ width: "268px" }}>
-            Dịch vụ sử dụng
-          </th>
-          <th className={classes.th} style={{ width: "82px" }}></th>
-          <th className={classes.th} style={{ width: "104px" }}></th>
-        </tr>
-        {listData.loading ? (
-          <p>Loading...</p>
-        ) : (
-          listData.lists.map((list: any, index: any) => (
-            <div>
-              <tr key={index}>
-                <td className={classes.td}>{list.deviceCode}</td>
-                <td className={classes.td}>{list.deviceName} </td>
-                <td className={classes.td}>{list.iP}</td>
-                <td className={classes.td}>
-                  <img src={chamdo} className={classes.chamdo} />
-                  {list.activeStatus}
-                </td>
-                <td className={classes.td}>
-                  <img src={chamdo} className={classes.chamdo} />
-                  {list.connecting}
-                </td>
-                <td className={classes.td}>
-                  <div>Khám tim mạch, Khám mắt...</div>
-                  <div className={classes.duongdan}>Xem thêm</div>
-                </td>
-                <td className={`${classes.td} ${classes.duongdan}`}>
-                  Chi tiết{" "}
-                </td>
-                <td className={`${classes.td} ${classes.duongdan}`}>
-                  Cập nhật
-                </td>
-              </tr>
-              {/* <tr key={index}>
+    <div>
+      <Link to="/device/adddevice">
+        <div className={classes.addnewdevice}>
+          <img src={addsquare} />
+          <div className={classes.addnewdevicecontent}>Thêm thiết bị</div>
+        </div>
+      </Link>
+      <div className={classes.trangthaihoatdong}>
+        <div className={classes.trangthaihoatdongname}>
+          Trạng thái hoạt động
+        </div>
+        <div className={`${classes.trangthaihoatdongselec} ${classes.trs}`}>
+          <Dropdown
+            options={options}
+            onChange={(e) => handleDropdownValue(e)}
+            value={defaultOption}
+            placeholder="Select an option"
+          />
+        </div>
+      </div>
+      <div className={classes.trangthaiketnoi}>
+        <div className={classes.trangthaihoatdongname}>Trạng thái kết nối</div>
+        <div className={`${classes.trangthaihoatdongselec} ${classes.trs}`}>
+          <Dropdown
+            options={optionsConnect}
+            onChange={(e) => handleDropdownValue(e)}
+            value={defaultOption}
+            placeholder="Select an option"
+          />
+        </div>
+      </div>
+      <div className={classes.tukhoa}>
+        <div className={classes.trangthaihoatdongname}>Từ khóa</div>
+        <input
+          placeholder="Nhập từ khóa"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className={classes.trangthaihoatdongselec}
+        />
+        <img src={search} className={classes.search} />
+      </div>
+      <section className={classes.table}>
+        <table className={classes.bang}>
+          <tr>
+            <th className={classes.th} style={{ width: "103px" }}>
+              Mã thiết bị
+            </th>
+            <th className={classes.th} style={{ width: "99px" }}>
+              Tên thiết bị
+            </th>
+            <th className={classes.th} style={{ width: "136px" }}>
+              Địa chỉ IP
+            </th>
+            <th className={classes.th} style={{ width: "174px" }}>
+              Trạng thái hoạt động
+            </th>
+            <th className={classes.th} style={{ width: "145px" }}>
+              Trạng thái kết nối
+            </th>
+            <th className={classes.th} style={{ width: "268px" }}>
+              Dịch vụ sử dụng
+            </th>
+            <th className={classes.th} style={{ width: "82px" }}></th>
+            <th className={classes.th} style={{ width: "104px" }}></th>
+          </tr>
+          {listData.lists.map((list: any, index: any) => (
+            <tr key={index}>
+              <td className={classes.td}>
+                <span>{list.deviceCode}</span>
+              </td>
+              <td className={classes.td}>
+                <span>{list.deviceName}</span>{" "}
+              </td>
+              <td className={classes.td}>
+                <span>{list.iP}</span>
+              </td>
+              <td className={classes.td}>
+                <span>
+                  {" "}
+                  {list.activeStatus == "Hoạt động" ? (
+                    <ServiceActivePoint />
+                  ) : (
+                    <ServiceUnactivePoint />
+                  )}
+                </span>
+              </td>
+              <td className={classes.td}>
+                <span>
+                  {" "}
+                  {list.connecting == "Kết nối" ? (
+                    <ServiceActivePoint1 />
+                  ) : (
+                    <ServiceUnactivePoint1 />
+                  )}
+                </span>
+              </td>
+              <td className={classes.td}>
+                <span>
+                  {" "}
+                  {list.serviceUse}
+                  {showMore && (
+                    <span>
+                      {" "}
+                      khám Sản - Phụ khoa, Khám răng hàm mặt, Khám tai mũi họng,
+                      Khám hô hấp, Khám tổng quát
+                    </span>
+                  )}
+                  {!showMore && (
+                    <>
+                      <br />
+                      <a href="" onClick={() => setShowMore(!showMore)}>
+                        {" "}
+                        Xem thêm
+                      </a>
+                    </>
+                  )}
+                </span>
+              </td>
+              <td className={`${classes.td} ${classes.duongdan}`}>Chi tiết </td>
+              <td className={`${classes.td} ${classes.duongdan}`}>Cập nhật</td>
+            </tr>
+            /* <tr key={index}>
                 <td className={classes.tdchan}>{list.deviceCode}</td>
                 <td className={classes.tdchan}>{list.deviceName}</td>
                 <td className={classes.tdchan}>{list.iP}</td>
@@ -86,145 +236,11 @@ function DeciveList({ listData, fetchLists }: any) {
                 <td className={`${classes.duongdan} ${classes.tdchan}`}>
                   Cập nhật
                 </td>
-              </tr> */}
-            </div>
-          ))
-        )}
-        {/* <tr>
-          <td className={classes.td}>KIO_01</td>
-          <td className={classes.td}> Kiosk</td>
-          <td className={classes.td}>192.168.1.10</td>
-          <td className={classes.td}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Hoạt động
-          </td>
-          <td className={classes.td}>
-            <img src={chamdo} className={classes.chamdo} />
-            Mất kết nối
-          </td>
-          <td className={classes.td}>
-            <div>Khám tim mạch, Khám mắt...</div>
-            <div className={classes.duongdan}>Xem thêm</div>
-          </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Chi tiết </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Cập nhật</td>
-        </tr>
-        <tr>
-          <td className={classes.tdchan}>KIO_01</td>
-          <td className={classes.tdchan}> Kiosk</td>
-          <td className={classes.tdchan}>192.168.1.10</td>
-          <td className={classes.tdchan}>
-            <img src={chamdo} className={classes.chamdo} />
-            Ngưng hoạt động
-          </td>
-          <td className={classes.tdchan}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Kết nối
-          </td>
-          <td className={classes.tdchan}>
-            <div>Khám tim mạch, Khám mắt...</div>
-            <div className={classes.duongdan}>Xem thêm</div>
-          </td>
-          <td className={`${classes.duongdan} ${classes.tdchan}`}>Chi tiết </td>
-          <td className={`${classes.duongdan} ${classes.tdchan}`}>Cập nhật</td>
-        </tr>
-        <tr>
-          <td className={classes.td}>KIO_01</td>
-          <td className={classes.td}> Kiosk</td>
-          <td className={classes.td}>192.168.1.10</td>
-          <td className={classes.td}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Hoạt động
-          </td>
-          <td className={classes.td}>
-            <img src={chamdo} className={classes.chamdo} />
-            Mất kết nối
-          </td>
-          <td className={classes.td}>
-            <div>Khám tim mạch, Khám mắt...</div>
-            <div className={classes.duongdan}>Xem thêm</div>
-          </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Chi tiết </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Cập nhật</td>
-        </tr>
-        <tr>
-          <td className={classes.tdchan}>KIO_01</td>
-          <td className={classes.tdchan}> Kiosk</td>
-          <td className={classes.tdchan}>192.168.1.10</td>
-          <td className={classes.tdchan}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Hoạt động
-          </td>
-          <td className={classes.tdchan}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Kết nối
-          </td>
-          <td className={classes.tdchan}>
-            <div>Khám tim mạch, Khám mắt...</div>
-            <div className={classes.duongdan}>Xem thêm</div>
-          </td>
-          <td className={`${classes.duongdan} ${classes.tdchan}`}>Chi tiết </td>
-          <td className={`${classes.duongdan} ${classes.tdchan}`}>Cập nhật</td>
-        </tr>
-        <tr>
-          <td className={classes.td}>KIO_01</td>
-          <td className={classes.td}> Kiosk</td>
-          <td className={classes.td}>192.168.1.10</td>
-          <td className={classes.td}>
-            <img src={chamdo} className={classes.chamdo} />
-            Ngưng hoạt động
-          </td>
-          <td className={classes.td}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Kết nối
-          </td>
-          <td className={classes.td}>
-            <div>Khám tim mạch, Khám mắt...</div>
-            <div className={classes.duongdan}>Xem thêm</div>
-          </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Chi tiết </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Cập nhật</td>
-        </tr>
-        <tr>
-          <td className={classes.tdchan}>KIO_01</td>
-          <td className={classes.tdchan}> Kiosk</td>
-          <td className={classes.tdchan}>192.168.1.10</td>
-          <td className={classes.tdchan}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Hoạt động
-          </td>
-          <td className={classes.tdchan}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Kết nối
-          </td>
-          <td className={classes.tdchan}>
-            <div>Khám tim mạch, Khám mắt...</div>
-            <div className={classes.duongdan}>Xem thêm</div>
-          </td>
-          <td className={`${classes.duongdan} ${classes.tdchan}`}>Chi tiết </td>
-          <td className={`${classes.duongdan} ${classes.tdchan}`}>Cập nhật</td>
-        </tr>
-        <tr>
-          <td className={classes.td}>KIO_01</td>
-          <td className={classes.td}> Kiosk</td>
-          <td className={classes.td}>192.168.1.10</td>
-          <td className={classes.td}>
-            <img src={chamxanh} className={classes.chamxanh} />
-            Hoạt động
-          </td>
-          <td className={classes.td}>
-            <img src={chamdo} className={classes.chamdo} />
-            Mất kết nối
-          </td>
-          <td className={classes.td}>
-            <div>Khám tim mạch, Khám mắt...</div>
-            <div className={classes.duongdan}>Xem thêm</div>
-          </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Chi tiết </td>
-          <td className={`${classes.td} ${classes.duongdan}`}>Cập nhật</td>
-        </tr> */}
-      </table>
-    </section>
+              </tr> */
+          ))}
+        </table>
+      </section>
+    </div>
   );
 }
 const mapStateToProps = (state: any) => {

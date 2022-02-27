c3eaef89dddb0631589ea38360dbce9372fc49d8
Huỳnh Tấn Sĩ <95234782+HuynhTanSi34@users.noreply.github.com>
2022-02-27 17:37:57 +0700

    Add files via upload

---
+++ src/redux/service/serviceAction.ts
@@ -0,0 +1,38 @@
+import { REQ, SUCCESS, ERROR } from "./serviceTypes";
+import db from "../../firebase.config";
+export const FETCHREQ = () => {
+  return {
+    type: REQ,
+  };
+};
+export const FETCHSUCCES = (lists: any) => {
+  return {
+    type: SUCCESS,
+    payload: lists,
+  };
+};
+export const FETCHERROR = (error: any) => {
+  return {
+    type: ERROR,
+    payload: error,
+  };
+};
+export const fetchLists = () => {
+  return (dispatch: any) => {
+    dispatch(FETCHREQ);
+    const response = db.collection("ser");
+    response
+      .get()
+      .then((response) => {
+        const lists: any = [];
+        response.docs.forEach((item) => {
+          lists.push(item.data());
+        });
+        dispatch(FETCHSUCCES(lists));
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        dispatch(FETCHERROR(errorMessage));
+      });
+  };
+};
---
+++ src/redux/user/userReducer.ts
@@ -0,0 +1,29 @@
+import { REQ, SUCCESS, ERROR } from "./userTypes";
+const initState = {
+  loading: true,
+  error: "",
+  lists: [],
+};
+const reducer = (state = initState, action: any) => {
+  switch (action.type) {
+    case REQ:
+      return {
+        ...state,
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        lists: action.payload,
+      };
+    case ERROR:
+      return {
+        ...state,
+        lists: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+export default reducer;
